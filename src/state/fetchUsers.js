2d922277c9edec59504e84b21020845dcf024216
Robert Piotrowski <piotrowskirobert7@gmail.com>
2018-08-15 14:08:40 +0200

    done

--- src/state/fetchUsers.js
+++ src/state/fetchUsers.js
@@ -1,5 +1,49 @@
-const initialState = 0
+const SET_USERS = './fetchUsers/SET_USERS'
+const USERS_STARTED_LOADING = './fetchUsers/USERS_STARTED_LOADING'
+const USERS_STOPPED_LOADING = './fetchUsers/USERS_STOPPED_LOADING'
+
+export const usersStartedLoading = () => ({type: USERS_STARTED_LOADING})
+export const usersStoppedLoading = () => ({type: USERS_STOPPED_LOADING})
+
+
+export const setUsersAction = data => ({
+    type: SET_USERS,
+    data
+})
+
+export const fetchUsersAction = () => (dispatch, getState) => {
+    dispatch(usersStartedLoading())
+    fetch('https://randomuser.me/api?results=10')
+    .then(response => response.json())
+    .then(data => {
+        dispatch(setUsersAction(data))
+        dispatch(usersStoppedLoading())
+    })
+}
+
+const initialState = {
+    users: null,
+    isUsersAreLoading: false
+}
 
 export default (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case SET_USERS:
+            return {
+                ...state,
+                users: action.data
+            }
+            case USERS_STARTED_LOADING:
+            return {
+                ...state,
+                isUsersAreLoading: true
+            }
+            case USERS_STOPPED_LOADING:
+            return {
+                ...state,
+                isUsersAreLoading: false
+            }
+        default:
+            return state
+    }
 }
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
-import fetchUsers from './state/fetchUsers'
+import fetchUsers, {fetchUsersAction} from './state/fetchUsers'
 
 const reducer = combineReducers({
     fetchUsers
@@ -10,3 +10,4 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
+store.dispatch(fetchUsersAction())
